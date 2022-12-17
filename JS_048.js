c376e7fee6bc31e24796dfcdd54eb7a50e6b69e3
IvanVlKuchumov <kuchumoviv@gmail.com>
2022-12-17 12:01:35 +0300

    Update JS_048.js

--- JS_048.js
+++ JS_048.js
@@ -2,4 +2,4 @@
 
 Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
 Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.*/
-const feast  = (beast, dish) => {return beast.split("")[0] === dish.split("")[0] && beast.split("")[beast.split("").length - 1] === dish.split("")[dish.split("").length - 1] ? true : false}
+const feast  = (beast, dish) => {return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1] ? true : false}
