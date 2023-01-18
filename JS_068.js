1485f39abcbd1e683b9667d80d40df61495d6091
IvanVlKuchumov <kuchumoviv@gmail.com>
2023-01-18 19:58:06 +0300

    Update JS_068.js

--- JS_068.js
+++ JS_068.js
@@ -1,4 +1,2 @@
 /*Given a number n, return the number of positive odd numbers below n, EASY!*/
-function oddCount(n){
-     return n % 2 === 0 ? n / 2 : (n - 1)/2
-}
+const oddCount = n => Math.floor(n/2) ;
