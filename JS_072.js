2ea55b7f22d56b5804f863c2f81ae974e1581b3a
IvanVlKuchumov <kuchumoviv@gmail.com>
2023-01-28 12:45:02 +0300

    Update JS_072.js

--- JS_072.js
+++ JS_072.js
@@ -1,4 +1,4 @@
 /*Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.*/
 function divisibleBy(numbers, divisor){
-  return numbers.filter(el=>el/divisor === Math.floor(el/divisor))
+  return numbers.filter(el=> el % divisor === 0)
 }
